3b85c97b859688d6cb8904248a502640208bd735
Diog007 <diogo.mz123@gmail.com>
2026-01-18 17:11:55 -0300

    ajustes

--- src/main/java/com/simulador/config/CorsConfig.java
+++ src/main/java/com/simulador/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
                         .allowedOrigins(
                             "https://mockflow.diogobluesec.tech", 
                             "https://diogobluesec.tech", 
-                            "https://app.diogobluesec.tech",
+                            "https://app.diogobluesec.tech"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
